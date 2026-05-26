b6134a04a4d9f06838b077090ee9763b98539022
ddm Trieste <ddm.trieste@gmail.com>
2026-05-26 14:57:57 +0100

    indroduced better detection w hough detection. test camera flir for other camera (NOT WORKING). Started working on ROI on test_capture_v3

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bead_python
 
 ICTP summer 2026 code for capture adn tracking images of spheres.
+
+
+### Camera model
+DELL pro webcam wb 5023
---
+++ hough_detection.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+max_slider = 10
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+cap.release()
+
+# Read image as gray-scale
+img = frame # cv2.imread('circles.png', cv2.IMREAD_COLOR)
+# Convert to gray-scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Blur the image to reduce noise
+img_blur = cv2.medianBlur(gray, 5)
+# Apply hough transform on the image
+circles = cv2.HoughCircles(img_blur, 
+                           cv2.HOUGH_GRADIENT, 1, img.shape[0]/64, 
+                           param1=200, param2=10, minRadius=1, maxRadius=5)
+# Draw detected circles
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # Draw outer circle
+        cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # Draw inner circle
+        cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv2.imshow('Hough Circle Detection', img)
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
